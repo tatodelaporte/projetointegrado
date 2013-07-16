43ab4c354eb96639ea5e18e04bdb7321cc3e3750
tatodelaporte <d.delaporte@hotmail.com>
2013-07-16 12:08:21 -0300

    #11 Criado a Classe Funcionario para trabalhar junto com a tela e o Dao
    Estou postando agora para REorganizar o GIT,

---
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/funcionarios/Funcionarios.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsi.projetointegrador.funcionarios;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Funcionarios {
+    
+    private int id;
+    private String Nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+    
+}
