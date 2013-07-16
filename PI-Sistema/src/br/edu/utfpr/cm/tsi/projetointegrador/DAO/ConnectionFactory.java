62dd6ae5c44014802cc5d3a553c8435d16dd3b68
tatodelaporte <d.delaporte@hotmail.com>
2013-07-16 12:10:17 -0300

    . Criado ConectionFactory para ganhar a conexão com o banco e criado o Dao Interface

---
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/DAO/ConnectionFactory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsi.projetointegrador.DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author suporte
+ */
+public class ConnectionFactory {
+    
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/swin?user=root&password=tato1234";
+
+    public static  Connection prepareConnection() throws SQLException {
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Connection con = DriverManager.getConnection(JDBC_URL);
+        con.setAutoCommit(true);
+        return con;
+    }
+    
+
+}
---
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/DAO/Dao.java
@@ -0,0 +1,18 @@
+package br.edu.utfpr.cm.tsi.projetointegrador.DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author Daniel
+ */
+public interface Dao<T> {
+
+    void persist(T o) throws Exception;
+
+    void delete(T o) throws Exception;
+
+    T retrieve(int id) throws Exception;
+
+    List<T> list() throws Exception;
+}
