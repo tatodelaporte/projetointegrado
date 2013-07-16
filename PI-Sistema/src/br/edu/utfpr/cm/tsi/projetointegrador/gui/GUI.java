6fa545023aaf8f2e000d5a4fed370c5cef0d31db
tatodelaporte <d.delaporte@hotmail.com>
2013-07-16 20:05:33 -0300

    #12  Tabela será gerada pela GUI ao iniciar o sistema
    Será mudado nas proximas versoes a onde criar o banco

--- PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/gui/GUI.java
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/gui/GUI.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.edu.utfpr.cm.tsi.projetointegrador.gui;
 
 import br.edu.utfpr.cm.tsi.projetointegrador.DAO.ConnectionFactory;
