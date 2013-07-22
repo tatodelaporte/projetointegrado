516e906ef367f141685ade6bb4913deb0aa0ffe7
tatodelaporte <d.delaporte@hotmail.com>
2013-07-21 22:53:41 -0300

    #17 Criado novos campos

--- PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/funcionario/Funcionario.java
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/funcionario/Funcionario.java
@@ -2,16 +2,23 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.utfpr.cm.tsi.projetointegrador.funcionarios;
+package br.edu.utfpr.cm.tsi.projetointegrador.funcionario;
 
 /**
  *
  * @author Daniel
  */
 public class Funcionario {
-    
+
     private int id;
-    private String Nome;
+    private String nome;
+    private String cpf;
+    private String rg;
+    private String cep;
+    private String prefixo;
+    private String endereco;
+    private String numero;
+    private String cidade;
 
     public int getId() {
         return id;
@@ -22,11 +29,141 @@ public class Funcionario {
     }
 
     public String getNome() {
-        return Nome;
+        return nome;
     }
 
-    public void setNome(String Nome) {
-        this.Nome = Nome;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
-    
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getPrefixo() {
+        return prefixo;
+    }
+
+    public void setPrefixo(String prefixo) {
+        this.prefixo = prefixo;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getFoneCelular() {
+        return foneCelular;
+    }
+
+    public void setFoneCelular(String foneCelular) {
+        this.foneCelular = foneCelular;
+    }
+
+    public String getFoneResidencia() {
+        return foneResidencia;
+    }
+
+    public void setFoneResidencia(String foneResidencia) {
+        this.foneResidencia = foneResidencia;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getTipoFuncionario() {
+        return TipoFuncionario;
+    }
+
+    public void setTipoFuncionario(String TipoFuncionario) {
+        this.TipoFuncionario = TipoFuncionario;
+    }
+    private String foneCelular;
+    private String foneResidencia;
+    private String uf;
+    private String TipoFuncionario;
+
+    public enum UF {
+
+        AC, AL, AP, AM, BA, CE, DF,
+        ES, GO, MA, MT, MS, MG, PA,
+        PB, PR, PE, PI, RJ, RN, RS,
+        RO, RR, SC, SP, SE, TO,}
+
+    public enum Prefixo {
+
+        ALAMEDA, AVENIDA, BECO, BLOCO, BOSQUE, CHACARA,
+        COLONIA, CONDOMINIO, CONJUNTO, DISTRITO, ESTRADA,
+        FAVELA, FAZENDA, FERROVIA, ILHA, JARDIM, LADEIRA, LAGO,
+        LAGOA, LOTEAMENTO, MORRO, PATIO, PONTE, PORTO, PRACA, PROLONGAMENTO,
+        QUADRA, RAMAL, RAMPA, RECANDO, RODOVIA, RUA, SITIO, TERMINAL, TRAVESSA,
+        TREVO, TUNEL, UNIDADE, VALA, VIA, VIADUTO, VIELA, VILA,}
+
+    public enum TipoFuncionario {
+
+        Atendente,
+        Professor,
+        Gerente,}
+
+    @Override
+    public String toString() {
+
+        return "Funcionario {" + "id=" + id + ","
+                + "nome" + nome + ","
+                + "cpf" + cpf + ","
+                + "rg" + rg + ","
+                + "cep" + cep + ","
+                + "prefixo" + prefixo + ","
+                + "endereco" + endereco + ","
+                + "cidade" + cidade + ","
+                + "uf" + uf + ","
+                + "fonecelular" + foneCelular + ","
+                + "foneresidencia" + foneResidencia + ","
+                + "tipofuncionario" + TipoFuncionario + '}';
+    }
 }
