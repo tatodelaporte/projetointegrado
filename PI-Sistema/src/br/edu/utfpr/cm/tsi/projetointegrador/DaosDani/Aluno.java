aef2b083ea370e0dce098a2bc38e006d59998737
daniLima <danielelyma@gmail.com>
2013-07-26 22:16:33 -0300

    # DaoAluno

---
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/DaosDani/Aluno.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsi.projetointegrador.DaosDani;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Daniele
+ */
+public class Aluno {
+    
+     private int id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String telefone;
+    private Date dataNascimento=Calendar.getInstance().getTime();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+      if(dataNascimento == null)
+                throw new RuntimeException("A data de nascimento nao pode ser NULL");
+        
+        this.dataNascimento = dataNascimento;
+    }
+
+    @Override
+    public String toString() {
+        return "Aluno{" + "id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", endereco=" + endereco + ", telefone=" + telefone + '}';
+    }
+
+ 
+    
+    
+}   
+    
+
---
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/DaosDani/DaoAluno.java
@@ -0,0 +1,119 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsi.projetointegrador.DaosDani;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Daniele
+ */
+public class DaoAluno {
+    
+    private static Aluno converteRsParaAluno(ResultSet rs) throws SQLException {
+        Aluno p = new Aluno();
+        p.setId(rs.getInt("id"));
+        p.setNome(rs.getString("nome"));
+        p.setCpf(rs.getString("cpf"));
+        p.setEndereco(rs.getString("endereco"));
+        p.setTelefone("telefone");
+        p.setDataNascimento(rs.getDate("dataNascimento"));
+      //  p.setDataNascimento(null);
+        
+        return p;
+    }
+        
+        private DaoAluno() {}
+            
+      private static final String JDBC_URL = "jdbc:mysql://localhost:3306/pisistemas?user=root&password=douglas";
+    
+      public static void insert(Aluno p) throws SQLException{      
+        PreparedStatement pst =prepareConnection().prepareStatement("INSERT INTO aluno (nome, cpf, endereco,telefone, dataNascimento) VALUES (?,?,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+        pst.setString(1, p.getNome());
+        pst.setString(2, p.getCpf());
+        pst.setString(3, p.getEndereco());
+        pst.setString(4, p.getTelefone());
+        pst.setDate(5, new Date(p.getDataNascimento().getTime()));
+        
+        pst.execute();
+        
+        ResultSet rs = pst.getGeneratedKeys();
+        rs.next();
+        p.setId(rs.getInt(1));
+    } 
+    
+    
+       public static Aluno retrieve(int id) throws SQLException{
+        Statement st =  prepareConnection().createStatement();                                
+        st.execute("SELECT * FROM aluno");
+        ResultSet rs = st.getResultSet();
+        
+        rs.next();
+        Aluno p = converteRsParaAluno(rs);
+       
+        return p;
+    }
+      
+      public static void update(Aluno p) throws SQLException{
+        PreparedStatement pst =  prepareConnection().prepareStatement("UPDATE aluno SET nome = ?, cpf = ?, endereco = ?, telefone = ? WHERE id = ?");
+        pst.setString(1, p.getNome());
+        pst.setString(2, p.getCpf());
+        pst.setString(3, p.getEndereco());
+        pst.setString(4, p.getTelefone());
+        pst.setInt(5, p.getId());
+        pst.execute();
+    }
+      
+      
+      public static void delete(Aluno p) throws SQLException{
+        Statement st =  prepareConnection().createStatement();                                
+        st.execute("DELETE FROM aluno WHERE id = " + p.getId());
+    }
+      
+    
+      public static List<Aluno> list() throws SQLException{
+        List<Aluno> alunos = new ArrayList<Aluno>();
+        
+        Statement st =  prepareConnection().createStatement();                                
+        ResultSet rs =  st.executeQuery("SELECT * FROM aluno");
+        
+        while(rs.next()){
+            Aluno p = converteRsParaAluno(rs);
+            alunos.add(p);
+        }
+        
+        return alunos;
+    }
+      
+      
+       private static  Connection prepareConnection() throws SQLException {
+        DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+        Connection con = DriverManager.getConnection(JDBC_URL);
+        con.setAutoCommit(true);
+        return con;
+    }
+}
+
+      
+      
+    
+        
+    
+    
+
+    
+
+
+    
+    
+
