76a7b2866405f31da7673afbec0fd3389aef035a
daniLima <danielelyma@gmail.com>
2013-08-18 22:57:07 -0300

    #8 CadastroPiscinas

---
+++ PI-Sistema/src/br/edu/utfpr/cm/tsi/projetointegrador/piscina/Piscina.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.cm.tsi.projetointegrador.piscina;
+
+/**
+ *
+ * @author Daniele
+ */
+public class Piscina {
+    private int id;
+    private String descricao;
+    private int capacidadeAlunos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCapacidadeAlunos() {
+        return capacidadeAlunos;
+    }
+
+    public void setCapacidadeAlunos(int capacidadeAlunos) {
+        this.capacidadeAlunos = capacidadeAlunos;
+    }
+
+    @Override
+    public String toString() {
+        return "Piscina{" + "id=" + id + ", descricao=" + descricao + ", capacidadeAlunos=" + capacidadeAlunos + '}';
+    }
+    
+    
+    
+}
